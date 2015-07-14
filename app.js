6aba0f6b1ebb91ce40e797e253a9fc12c221e697
Miles Hochstein <miles.hochstein@gmail.com>
2015-07-14 15:59:48 -0700

    set up /show endpoint that returns full database json

--- app.js
+++ app.js
@@ -1,43 +1,13 @@
- /* POST to Add User Service */
- 
-
-
-// router.post('/update', function(req,res){
-//     console.log('request body id is '+ req.body.id);   
-//     console.log('username is  '+ req.body.username);   
-    
-//     User.findOneAndUpdate(
-//                 { _id: req.body.id}, 
-//                 {$set: {
-//                         _id         : req.body.id,
-//                         postname    : req.body.postname,
-//                         username    : req.body.username,
-//                         email       : req.body.email,
-//                         organization: req.body.organization,
-//                         telephone   : req.body.telephone,
-//                         postcontent : req.body.postcontent
-//                 }}, 
-//                 {upsert: false},
-
-//                 function(err, docs){
-//                      res.redirect('userlist');
-//                     }
-//                 );
-//  });
-
-
-// START THE SERVER
-// =============================================================================
- 
-//https://scotch.io/tutorials/build-a-restful-api-using-node-and-express-4
-var express = require('express');
+var express = require('express'); 
 var app = express();
 var router = express.Router();
 var bodyParser = require('body-parser');
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public')); //
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/api',router);  //move this from bottom to here
 
 //database connection
 var mongoose = require('mongoose');
@@ -52,7 +22,7 @@ try {
 catch(err){
     //console.log("no connection file so go on to Heroku config var");
     //var uristring = process.env.MONGOLAB_URI;   //if Heroku env
-}
+    }   
 console.log("Either way: uristring is "+ uristring);
 
 var db = mongoose.connect(uristring);
@@ -87,9 +57,37 @@ router.use(function(req, res, next) {
 });
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.json({ message: 'hooray! welcome to our api!' });
-});
+    // router.get('/', function(req, res) {
+    //   res.json({ message: 'hooray! welcome to our api!' });
+    // });
+
+app.get('/show', function(req,res,next){
+    Day.find(function(err, Days){
+        if (err) 
+            {return next(err)}
+        else
+            {
+            //res.json({message: ' that worked '})  return success message
+            console.log("we good");
+            console.log(Days[30]);
+            res.status(201).json(Days); //returns saved Days object
+            //res.sendFile(__dirname + '/public/show.html')  //returns show.html
+            }
+    })
+
+    //res.sendFile(__dirname + '/public/show.html')
+    //res.json({ message: 'hooray! welcome to our api!' });
+})
+
+/* GET home page. */
+// router.get('/show', function(req, res, next) {
+//    //console.log('SHOW is happening.');
+//    res.json({ message: 'here will be a list of trips' });
+
+//     res.sendFile('./show.html');
+
+// });
+
 
 /* POST to Add Trip Service */
 router.route('/addday').post(function(req, res) {
@@ -114,7 +112,6 @@ router.route('/addday').post(function(req, res) {
      })
   });
 
-app.use('/api',router);
 
 //module.exports = router;
 app.listen(3000);
