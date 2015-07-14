0007084bf2e24c5c0c18184f623ceec20368b1df
Wendy Beck <webeck@gmail.com>
2015-07-13 17:12:24 -0700

    Refining controllers and services

--- build/js/app/controllers/locationController.js
+++ build/js/app/controllers/locationController.js
@@ -1,11 +1,19 @@
 angular.module('dayBreak').controller('locationController',['$http','locationService',function($http,locationService){
-console.log('in loco controller');
+	var locName; 
+	//this.locURL = 'http://www.google.com';
 
-this.enterName = function() {
-	var name=this.locationName;
-	console.log('hello location name: ',name);
-	locationService.getName(name);
-	//tripService.addDay(name);
+this.locName = function() {
+	locName = this.locationName;
+	console.log(locName);
+	locationService.getName(locName);
+
 };
+
+this.locURL = function(locName){
+	console.log('in locURL')
+	locationService.getURL(locName);
+
+};
+
 
 }]);
