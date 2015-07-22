a3d675edd6f0000514a635ede44c3a27d691706a
Wendy Beck <webeck@gmail.com>
2015-07-22 13:28:04 -0700

    nested forms in progress

--- build/js/app/controllers/locationController.js
+++ build/js/app/controllers/locationController.js
@@ -1,19 +1,35 @@
-angular.module('dayBreak').controller('locationController',['$http','locationService',function($http,locationService){
-	var locName; 
-	//this.locURL = 'http://www.google.com';
+angular.module('dayBreak').controller('locationController',['$http','$scope',function($http,$scope){
+	$scope.locName = '';
+	$scope.locURL = '';
 
-this.locName = function() {
-	locName = this.locationName;
-	console.log(locName);
-	locationService.getName(locName);
+	//TODO: fix latlong so that it is bound to US not Australia
+	var defaultBounds = new google.maps.LatLngBounds(
+  		new google.maps.LatLng(-33.8902, 151.1759),
+  		new google.maps.LatLng(-33.8474, 151.2631));
 
-};
+	var input = document.getElementById('pac-input');
 
-this.locURL = function(locName){
-	console.log('in locURL');
-	locationService.getURL(locName);
+	var searchBox = new google.maps.places.SearchBox(input, {
+  		bounds: defaultBounds
+	});
 
-};
+	google.maps.event.addListener(searchBox, 'places_changed', function() {
+    	var places = searchBox.getPlaces();
 
+	$scope.locName = places[0].name;
+	$scope.locURL = places[0].website;
+	$scope.$apply();
+});
+	// $scope.FORMS = [
+	// // {locName:''}
+	//  ];
+
+	this.addLoc = function(Location) {
+console.log('Location: ',Location);
+		var locName=this.locName;
+		console.log('adding another location: ',locName);
+
+
+	}
 
 }]);
