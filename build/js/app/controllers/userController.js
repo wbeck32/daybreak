33c8bcfb9d6780ddf4053ff67fb48cac1746e6ff
Miles Hochstein <miles.hochstein@gmail.com>
2015-07-28 22:20:43 -0700

    added user about and reworked userViewSwitch

--- build/js/app/controllers/userController.js
+++ build/js/app/controllers/userController.js
@@ -16,6 +16,7 @@ angular.module('dayBreak')
   	this.showPasswordChange = false;
   	this.showDeleteAccount 	= false;
 
+
  var updateScope = function() {
     this.username 	= userService.username;
     this.userState 	= userService.userState;
@@ -23,6 +24,8 @@ angular.module('dayBreak')
     this.email 				= null;
     this.passwordConfirm 	= null;
     this.newPassword 		= null;
+    this.userViewSwitch = 'None';
+
   }
   .bind(this);//TODO: understand why this is needed
 
@@ -31,7 +34,9 @@ this.registerUser = function() {
 	
 	console.log("registering... this.username" + this.username);
 	console.log("this.password: " + this.password);
+	console.log("this.passwordConfirm: " + this.passwordConfirm);
 	console.log("this.email: " 	+ this.email);
+	
 	console.log("updateScope: " + updateScope);
 
 	userService.checkUsername(this.username);
@@ -61,11 +66,19 @@ this.toggleLoginLogout = function(){
 	updateScope();
 };
 
+
 this.checkUsername = function(){
 	console.log("checking for unique user name...");
-
-	userService.checkUsername();
-	updateScope();
+	if (this.username)
+	{
+		userService.checkUsername(this.username, duplicateUserName);
+		updateScope();
+	}
+	else
+	{
+	 	userService.duplicateUserName = null;
+	 	dpublicateUsername();	
+	}
 };
 
 
