565e45d466d2a1728d8c4e7b193b923849ddd25a
Miles Hochstein <miles.hochstein@gmail.com>
2015-09-01 17:45:06 -0700

    single versus grid view

--- build/js/app/controllers/userController.js
+++ build/js/app/controllers/userController.js
@@ -1,10 +1,8 @@
 dayBreak.controller('userController',
 		['$scope', '$rootScope','$http', 'userService', function($scope,$rootscope, $http,userService){
 		
-
-		$scope.User.testing 	= "testing testing";
-
 		$scope.User.userFormView = 'hide';    		
+		$scope.User.userDayView =  "grid";  //user can change to 'single'
 
 		$scope.User.userViewSwitch 	= null;
  
@@ -19,10 +17,8 @@ dayBreak.controller('userController',
 		$scope.User.uniqueEmail 	= '';  // email is unique in database
   
 		$scope.matchingPassword		= '';
-		//$scope.validPassword = userService.validPassword; 
 
-		//$scope.User.longUsername 	= true;
-		$scope.User.uniqueUserName 	= '';
+ 		$scope.User.uniqueUserName 	= '';
 		$scope.User.longUsername	= false;
 
 		$scope.User.validPassword = false; 
@@ -31,15 +27,8 @@ dayBreak.controller('userController',
 
 		$scope.User.username = userService.username;	    
 		$scope.User.userState 			= userService.userState;
-		//$rootscope.User.userState 			= userService.userState;
-//		$scope.User.userState 			= 'loggedOut';
-		
-		// this.userRegister 		= userService.userRegister; //mh
-		// this.showPasswordChange = false;
-		// this.showDeleteAccount 	= false;
  
  
-
 //////////////////////////////////////////////////////
 	//on keyup test password for match and other criteria.
 	this.passconfirm = function(pass, passconfirm){
